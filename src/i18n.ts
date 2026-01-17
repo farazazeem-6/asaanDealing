28fe3f64545ee5dadfb514895b770e71b3eab45c
faraz-dhillon <faraz.tacticminds@gmail.com>
2026-01-17 17:31:20 +0500

    Resolved all the comments

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,32 +1,42 @@
 'use client';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
 import enTranslation from './locales/en/translation.json';
 import urTranslation from './locales/ur/translation.json';
 import romanTranslation from './locales/roman/translation.json';
 
 if (!i18n.isInitialized) {
-  i18n.use(initReactI18next).init({
-    resources: {
-      en: {
-        translation: enTranslation,
+  i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+      resources: {
+        en: {
+          translation: enTranslation,
+        },
+        ur: {
+          translation: urTranslation,
+        },
+        roman: {
+          translation: romanTranslation,
+        },
       },
-      ur: {
-        translation: urTranslation,
+      fallbackLng: 'en',
+      
+      detection: {
+        order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
+        caches: ['localStorage', 'cookie'],
       },
-      roman: {
-        translation: romanTranslation,
+
+      interpolation: {
+        escapeValue: false,
       },
-    },
-    lng: 'en',
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false,
-    },
-    react: {
-      useSuspense: false,
-    },
-  });
+      react: {
+        useSuspense: false,
+      },
+    });
 }
 
 export default i18n;
