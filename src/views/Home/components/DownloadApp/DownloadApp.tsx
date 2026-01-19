4d6f7c5d1f18540c10b33f109e789841d9911028
faraz-dhillon <faraz.tacticminds@gmail.com>
2026-01-19 12:43:02 +0500

    add headings in download app section

--- src/views/Home/components/DownloadApp/DownloadApp.tsx
+++ src/views/Home/components/DownloadApp/DownloadApp.tsx
@@ -1,14 +1,31 @@
 import {
   ComingSoonSection,
+  ContentHeading,
   ContentSection,
+  ContentSubHeading,
+  IllustrationImg,
   IllustrationSection,
 } from './style';
 
 export const DownloadApp = () => {
   return (
     <ComingSoonSection>
-      <ContentSection></ContentSection>
-      <IllustrationSection></IllustrationSection>
+      <ContentSection>
+        <ContentHeading>We Are Coding</ContentHeading>
+        <ContentSubHeading>
+          Get ready for an exciting new app experience!
+        </ContentSubHeading>
+        <ContentSubHeading>
+          We are working hard to bring you an amazing mobile experience.
+        </ContentSubHeading>
+      </ContentSection>
+      <IllustrationSection>
+        <IllustrationImg
+          height={500}
+          src="/images/AppDevelopment.svg"
+          alt="App Preview"
+        />
+      </IllustrationSection>
     </ComingSoonSection>
   );
 };
--- src/views/Home/components/DownloadApp/style.ts
+++ src/views/Home/components/DownloadApp/style.ts
@@ -1,7 +1,16 @@
-import { Box, Flex, HtmlImage } from '@/components/elements';
+import { Box, Flex, HtmlImage, Text } from '@/components/elements';
 import { styled } from '@/theme';
 
-export const ComingSoonSection = styled(Flex, {});
-export const ContentSection = styled(Flex, {});
+export const ComingSoonSection = styled(Flex, {
+  padding: '$rem$2 $rem$6_25',
+});
+export const ContentSection = styled(Flex, {
+  flexDirection: 'column !important',
+  backgroundColor:'$slate3'
+});
+export const ContentHeading = styled(Text, {});
+export const ContentSubHeading = styled(Text, {});
 export const IllustrationSection = styled(Box, {});
-export const IllustrationImg = styled(HtmlImage, {});
+export const IllustrationImg = styled(HtmlImage, {
+  border: '1px solid black',
+});
